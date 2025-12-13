44c95e0c7b7e7b073b6341a24a3beabd78074555
Ananya Shukla <annayashukla.ss290@gmail.com>
2025-12-13 19:16:29 +0530

    crudAPi

--- src/app.js
+++ src/app.js
@@ -4,15 +4,9 @@ const User = require("./Model/user")
 
 const  app = express();
 
+app.use(express.json());
 app.post("/signUp", async (req,res)=>{
-    const userObj = new User({
-        firstName:"Ananya",
-        lastName :"Shukla",
-        emailId :"a@gmail.com",
-        password:"a123@",
-        age:23,
-        gender:"Female"
-    })
+    const userObj = new User(req.body);
 try{
     await userObj.save();
     res.status(201).json({message:"user signed up successfully"})
@@ -22,7 +16,53 @@ catch(err){
 }
 })
 
+app.get("/getApi", async (req,res)=>{
+    const useremail = req.body.emailId;
+    console.log(useremail)
 
+    const user = await User.find({emailId:useremail})
+if(user.length === 0){
+    res.status(404).send("User not found");
+}
+{
+    res.send(user)
+}
+    }
+
+)
+
+app.delete("/deleteUser/", async(req,res)=>{
+    const userid = req.body._id
+    console.log(userid)
+    try{
+       
+        await User.deleteOne({_id:userid});
+        console.log(userid)
+        res.status(200).json({message:"user deleted successfully",userid})
+        
+        
+    }catch(err){
+        res.status(404).json(err.message,"user deletion failed !!")
+    }
+
+
+
+})
+
+app.put("/updateUser",async (req,res)=>{
+    const userid = req.body._id
+    const data = req.body
+
+    try{
+        await User.updateOne({_id:userid},data)
+        res.status(201).send("user updated Successfully")
+
+
+    }
+    catch(err){
+        res.status(404).send("Error in Update",err.message )
+    }
+})
 
 
 
