b802072a80eb523db84753bf1457ad927ac59065
Ananya Shukla <annayashukla.ss290@gmail.com>
2025-12-14 00:24:00 +0530

    API level data sanitiztion

--- src/Model/user.js
+++ src/Model/user.js
@@ -1,26 +1,61 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     firstName : {
-    type:String
+    type:String,
+    required :true,
+    unique:true,
+    minlength : 2,
+    maxlength:20
   },
   lastName:{
-    type:String
+    type:String,
+      required :true,
   },
   emailId:{
-    type:String
+    type:String,
+      required :true,
+      lowercase:true,
+      unique :true,
   },
   password:{
-    type:String
+    type:String,
+      required :true,
+      minlength : 9,
+    maxlength:50
   },
   age:{
-    type:Number
+    type:Number,
+    min:1,
+    max:120
   },
   gender:{
-    type:String
+    type:String,
+    lowercase:true,
+    validate: {
+    validator: function (value) {
+      return value === "male" || value === "female" || value === "other";
+    },
+    message: "gender must be male, female or other"
   }
-}
+},
+  skills:{
+    type:[]
+  },
+  about:{
+    type:String,
+    default:"this is deafult about for user",
+    maxlength:250,
+    
+  },
+  image:{
+    type:String,
+    default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbTu4diIJv94P-8WTWuUSn3zhn5oU5flJmQA&s"
  
+  }
+},
+{timestamps:true}
 
 )
 
--- src/app.js
+++ src/app.js
@@ -1,82 +1,85 @@
-const express = require('express');
-const connectDB = require('./config/database');
-const User = require("./Model/user")
+const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./Model/user");
 
-const  app = express();
+const app = express();
 
 app.use(express.json());
-app.post("/signUp", async (req,res)=>{
-    const userObj = new User(req.body);
-try{
+app.post("/signUp", async (req, res) => {
+  const userObj = new User(req.body);
+  try {
     await userObj.save();
-    res.status(201).json({message:"user signed up successfully"})
-}  
-catch(err){
-    res.status(404).json(err.message,"user signup failed !!")
-}
-})
-
-app.get("/getApi", async (req,res)=>{
-    const useremail = req.body.emailId;
-    console.log(useremail)
-
-    const user = await User.find({emailId:useremail})
-if(user.length === 0){
-    res.status(404).send("User not found");
-}
-{
-    res.send(user)
-}
-    }
-
-)
-
-app.delete("/deleteUser/", async(req,res)=>{
-    const userid = req.body._id
-    console.log(userid)
-    try{
-       
-        await User.deleteOne({_id:userid});
-        console.log(userid)
-        res.status(200).json({message:"user deleted successfully",userid})
-        
-        
-    }catch(err){
-        res.status(404).json(err.message,"user deletion failed !!")
-    }
-
-
-
-})
-
-app.put("/updateUser",async (req,res)=>{
-    const userid = req.body._id
-    const data = req.body
-
-    try{
-        await User.updateOne({_id:userid},data)
-        res.status(201).send("user updated Successfully")
-
-
-    }
-    catch(err){
-        res.status(404).send("Error in Update",err.message )
-    }
-})
-
-
-
-connectDB().then(()=>{
-    console.log("database connection done succesfully")
-app.listen(7000, ()=>{
-    console.log("server start....")
+    res.status(201).json({ message: "user signed up successfully" });
+  } catch (err) {
+    res.status(404).json(err.message, "user signup failed !!");
+  }
 });
 
+app.get("/getApi", async (req, res) => {
+  const useremail = req.body.emailId;
+  console.log(useremail);
 
+  const user = await User.find({ emailId: useremail });
+  if (user.length === 0) {
+    res.status(404).send("User not found");
+  }
+  {
+    res.send(user);
+  }
+});
 
+app.delete("/deleteUser/", async (req, res) => {
+  const userid = req.body._id;
+  console.log(userid);
+  try {
+    await User.deleteOne({ _id: userid });
+    console.log(userid);
+    res.status(200).json({ message: "user deleted successfully", userid });
+  } catch (err) {
+    res.status(404).json(err.message, "user deletion failed !!");
+  }
+});
 
-}).catch(()=>{
-    console.log("connection not done succesfully")
-})
+app.patch("/updateUser/:_id", async (req, res) => {
+  const userid = req.params._id;
+  const data = req.body;
 
+  try {
+    const ALLOWED_UPDATES = [
+      "firstName",
+      "lastName",
+      "password",
+      "age",
+      "gender",
+      "skills",
+      "profile",
+      "about",
+      "image",
+    ];
+    const requestedUpdates = Object.keys(data).every((update)=>
+      ALLOWED_UPDATES.includes(update)
+    );
+    if (!requestedUpdates) {
+      return res.status(400).send("Invalid Updates");
+    }
+    if(data.skills.length >15){
+        throw new Error("skills cant be more than 15");
+    }
 
+    await User.updateOne({ _id: userid }, data, { returnDocuments: "after" });
+    res.status(201).send("user updated Successfully" + JSON.stringify(data));
+  } catch (err) {
+    res.status(404).send("Error in Update", err.message);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("database connection done succesfully");
+    app.listen(7000, () => {
+      console.log("server start....");
+    });
+  })
+  .catch(() => {
+    console.log("connection not done succesfully");
+  });
